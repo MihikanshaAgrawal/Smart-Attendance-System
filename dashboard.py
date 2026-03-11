cb40dc39922b63d5bc27964d585ed3e62728f827
Narayan-khatri826 <narayan.23ece305@rtu.ac.in>
2026-03-11 23:41:15 +0530

    updated-code

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,29 @@
 import streamlit as st
 import pandas as pd
+import os
 
-st.title("Attendance Dashboard")
+st.title("📊 Smart Attendance Dashboard")
 
-df = pd.read_csv("attendance.csv")
+if not os.path.exists("attendance.csv"):
+    st.warning("No attendance data found")
 
-st.dataframe(df)
+else:
+
+    df = pd.read_csv("attendance.csv")
+
+    st.subheader("Attendance Table")
+    st.dataframe(df)
+
+    st.subheader("Student Attendance Count")
+
+    # count attendance per student
+    count = df["Name"].value_counts().reset_index()
+
+    count.columns = ["Student Name", "Attendance Count"]
+
+    # show numbers clearly
+    st.table(count)
+
+    st.subheader("Attendance Graph")
+
+    st.bar_chart(count.set_index("Student Name"))
